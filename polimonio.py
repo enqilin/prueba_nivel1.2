66476b77bff26153e036d1e49aed7c7d35cbebe7
nadous <114181724+enqilin@users.noreply.github.com>
2023-03-12 22:51:24 +0000

    eliminar

--- polimonio.py
+++ polimonio.py
@@ -4,23 +4,8 @@ import csv
 
 class Nodo(object):
     """Clase nodo simplemente enlazado."""
-    info, sig= None, None
-    aux= Nodo()
-    aux.info = "Primer nodo"
-    palabra = input('Ingrese una palabra: ')
-    naux= aux
-    while (palabra != ""):
-        nodo = Nodo()
-        nodo.info = palabra
-        nodo.sig = nodo 
-        naux = nodo
-        palabra = input('Ingrese una palabra: ')
     
-    while (aux is not None):
-        print(aux.info)
-        aux = aux.sig
-
-
+    info, sig= None, None
 
 class datoPolinomio(object):
     """Clase dato polinomio"""
@@ -32,6 +17,7 @@ class datoPolinomio(object):
 
 class Polinomio(object):
     """clase polimonio"""
+
     def __init__(self) :
         """crear un polinomio del grado cero."""
         self.termino_mayor = None
@@ -66,7 +52,7 @@ class Polinomio(object):
         aux = polinomio.termino_mayor
         while(aux is not None and aux.info.termino > termino):
             aux = aux.sig 
-        if (aux is not None and aux.info.termino = termino):
+        if (aux is not None and aux.info.termino == termino):
             return aux.info.valor
         else:
             return 0
@@ -89,9 +75,9 @@ class Polinomio(object):
         paux = Polinomio()
         mayor = polinomio1 if (polinomio1.grado > polinomio2.grado) else polinomio2
         for i in range(0, mayor.grado+1):
-            total= obtener_valor(polinomio1, i ) + obtener_valor(polinomio2,i)
+            total= polinomio1.obtener_valor(i ) + polinomio2.obtener_valor(i)
             if (total != 0):
-                agregar_termino(paux, i total)
+                paux.agregar_termino( i, total)
         return paux
     
     def multiplicar(polinomio1, polinomio2):
@@ -102,19 +88,27 @@ class Polinomio(object):
             pol2 = polinomio2.termino_mayor
             while(pol2 is not None):
                 termino = pol1.info.termino + pol2.info.termino
-                valor = pol1.info.valoe + pol2.info.valor
-                if (obtener_valor(paux ,termino) != 0):
-                    valor += obtener_valor(paux , termino)
-                    modificar_termino(paux,termino,valor)
+                valor = pol1.info.valoe * pol2.info.valor
+                if (paux.obtener_valor(termino) != 0):
+                    valor += paux.obtener_valor( termino)
+                    paux.modificar_termino(termino,valor)
                 else:
-                    agregar_termino(paux,termino,valor)
+                    paux.agregar_termino(termino,valor)
                 pol2 = pol2.sig
             pol1 = pol1.sig
         return paux
         
-    def eliminar_termino(polinomio,termino,valor):
-        aux
+    def eliminar_termino(polinomio,termino):
+        aux = polinomio.termino.mayor
+        while( aux is not None and aux.info.termino > termino):
+            aux= aux.sig
+            if (aux is not None and aux.info.termino == termino):
+                aux=aux.sig
 
-    def polinomio_existe_termino(polinomio,termino,valor):
-
+    def polinomio_existe_termino(polinomio,termino):
+        aux = polinomio.termino_mayor
+        while (aux is not None and aux.info.termino > termino):
+            aux = aux.sig
+            if (aux is not None aux.info.termino == termino):
+                aux = 
 
